77f16b898c371a8a05b5577c7abb43f4fb23334b
Ari Olvera <badillo.ari@gmail.com>
2021-06-01 11:26:42 -0500

    feat: chapter 11 dates

---
+++ 12. dates_times/timestamps_comparison.js
@@ -0,0 +1,20 @@
+// timestamps
+
+const before = new Date('February 1 2019 7:30:59')
+const now = new Date()
+
+// console.log(now.getTime(), before.getTime())
+
+const diff = now.getTime() - before.getTime()
+console.log('diff', diff)
+
+const mins = Math.round(diff / 1000 / 60)
+const hours = Math.round(mins / 60)
+const days = Math.round(hours / 24)
+console.log(mins, hours, days)
+
+console.log(`the blog was written ${days} ago`)
+
+// converting timestamps into date objects
+const timestamp = 73534501633
+console.log(new Date(timestamp))
